c41e7006cb4057366b05663255e99c2531694a32
maatanyy <maatanyy@naver.com>
2021-10-18 22:36:22 +0900

    2차 176부터 수정해야함

--- exampleFile.c
+++ exampleFile.c
@@ -25,14 +25,14 @@ int main(void)
 {
 	Node *rootNode = (Node *)malloc(sizeof(Node)); //로트노드 동적할당 해주고 키 값 설정 및 링크 , 색 설정
 	rootNode->key = -10;
-	rootNode->right = NULL;
-	rootNode->left = NULL;
+	rootNode->right = NIL;
+	rootNode->left = NIL;
 	rootNode->color = BLACK;
 
 	NIL = (Node *)malloc(sizeof(Node)); //NIL 동적할당해주고 color BLACK으로 선언, RBT의 마지막 리프노드
 	NIL->color = BLACK;
 
-	FILE *pFile = fopen("test.txt", "r"); //read mode
+	FILE *pFile = fopen("test2.txt", "r"); //read mode
 	int intTemp = -1;
 
 	if (pFile == NULL)
@@ -53,28 +53,28 @@ int main(void)
 		// 트리생성
 		Node *newNode = (Node *)malloc(sizeof(Node));
 		newNode->key = intTemp;
-		newNode->right = NULL;
-		newNode->left = NULL;
+		newNode->right = NIL;
+		newNode->left = NIL;
 
 		if (rootNode->key == -10) // 루트노드가 없는 경우 즉 처음인 경우
 		{
 			rootNode = newNode; // 처음 들어오는 걸 루트로 설정해줌 이 test.txt의 경우 526
 		}
 
-		else //루트노드가 있는 경우
+		else if (rootNode->key != -10) // 루트노드가 있는 경우
 		{
 			InsertNode(&root, newNode); //이진트리 처럼 노드의 위치를 찾아 삽입
 			newNode->color = RED;		//삽입한 노드의 색을 RED로 해줍니다
 			newNode->left = NIL;		//노드의 left, right를 NIL로 설정해줍니다
 			newNode->right = NIL;
 			InspectNode(&root, newNode); //RED_BLACK_TREE를 깨나 검사해줍니다
+										 //printf("--");
 		}
 		// 생성된 트리에서 키 검색
 		printf("%d ", intTemp);
 	}
 
 	//파일 형태로 반환
-
 	fclose(pFile); //close file
 	return 0;
 }
@@ -112,59 +112,117 @@ void InsertNode(Node **rootNode, Node *newNode) //노드를 삽입하는 함수
 
 void InspectNode(Node **rootNode, Node *newNode) //노드를 검사하는 함수
 {
+
 	Node *root = (*rootNode);
 	Node *grandparent = newNode->parent->parent; //삽입된 노드의 부모의 부모를 grandparent로 설정해줍니다
+
+	if (root->color == 1)
+	{
+		root->color = 0;
+	}
 	//삽입될 노드의 부모가 BLACK 일 경우는 문제가 되지 않으므로 삽입될 노드의 부모가 RED일 경우만 고려해줍니다
+
+	//printf("Insepct");
 
 	if (newNode->parent->color == 1) //삽입될 노드의 부모가 RED 노드일 경우 문제가 발생한다
 	{
-
 		Node *other = newNode->parent->parent->right; //부모의 부모의 오른쪽 자식을 other로 해줍니다
 		Node *other2 = newNode->parent->parent->left; //부모의 부모의 왼쪽 자식을 other2로 해줍니다
 
-		//Root 기준 왼쪽으로 newNode가 들어오는 경우
-		//Case 1: other 가 RED인 경우
-		if (other->color == 1)				//만약 부모의 부모의 오른쪽 자식 색이 RED 일 경우
-		{									//case1
-			newNode->parent->color = BLACK; //삽입될 노드의 부모를 BLACK으로 바꿔줍니다
-			other->color = BLACK;
-			InspectNode(&root, newNode->parent->parent); // 위로 올라가며 문제가 있을 시 재귀적으로 다시 수정
-		}
-
-		if ()
-
-			//Case 2-1
-			else if ((newNode->parent->right == newNode) && (other->color == 0))
-			{ //조부모 다른 자식이 검정이고 삽입노드가 부모노드의 오른쪽 자식인경우
-				LeftRotate(&root, newNode);
-			}
-
-		//Case 2-2
-		else if ((newNode->parent->left == newNode) && (other->color == 0))
-		{ //조부모 다른 자식이 검정이고 삽입노드가 부모노드의 왼쪽 자식인경우
+		if (grandparent->right == NIL)
+		{
+			//printf("1");
 			RightRotate(&root, newNode);
+			newNode->parent->color = BLACK;
+			newNode->parent->right->color = RED;
 		}
 
-		//Root 기준 오른쪽으로 newNode가 들어오는 경우
-		//Case 1: other2 가 RED인 경우
-		if (other2->color == 1)				//만약 부모의 부모의 오른쪽 자식 색이 RED 일 경우
-		{									//case1
-			newNode->parent->color = BLACK; //삽입될 노드의 부모를 BLACK으로 바꿔줍니다
-			other2->color = BLACK;
-			InspectNode(&root, newNode->parent->parent); // 위로 올라가며 문제가 있을 시 재귀적으로 다시 수정
-		}
-
-		//Case 2-1
-		else if ((newNode->parent->right == newNode) && (other2->color == 0))
-		{ //조부모 다른 자식이 검정이고 삽입노드가 부모노드의 오른쪽 자식인경우
+		else if ((newNode = newNode->parent->left) && ((other != NIL) && (other->color == 0))) // Case 2-2
+		{
+			//printf("2");
 			RightRotate(&root, newNode);
+			newNode->parent->color = BLACK;
+			newNode->parent->left->color = RED;
 		}
 
-		//Case 2-2
-		else if ((newNode->parent->left == newNode) && (other2->color == 0))
-		{ //조부모 다른 자식이 검정이고 삽입노드가 부모노드의 왼쪽 자식인경우
+		else if ((newNode = newNode->parent->right) && ((other != NIL) && (other->color == 0))) //Case 2-1
+		{
+			//printf("3");
 			LeftRotate(&root, newNode);
+			InspectNode(&root, newNode->left);
 		}
+
+		else if (other->color == 1)
+		{
+			//printf("4");
+			newNode->parent->color = 0;
+			other->color = 0;
+			grandparent->color = 1;
+			InspectNode(&root, grandparent);
+		}
+
+		//printf("5");
+		// other2 즉 사촌이 왼쪽인 경우
+		else if (grandparent->left == NIL)
+		{
+			LeftRotate(&root, newNode);
+			newNode->parent->color = BLACK;
+			newNode->parent->left->color = RED;
+		}
+
+		else if ((newNode = newNode->parent->left) && ((other2 != NIL) && (other2->color == 0))) // Case 2-2
+		{
+
+			//여기 고쳐야함
+			RightRotate(&root, newNode);
+			//newNode->color = BLACK;
+			//newNode->parent->parent->color = RED;
+			InspectNode(&root, grandparent->right);
+		}
+
+		else if ((newNode = newNode->parent->right) && ((other2 != NIL) && (other2->color == 0))) //Case 2-1
+		{
+			LeftRotate(&root, newNode);
+			InspectNode(&root, newNode->left);
+		}
+
+		else if (other2->color == 1) //other2의 color가 red
+		{
+			newNode->parent->color = 0;
+			other2->color = 0;
+			grandparent->color = 1;
+			InspectNode(&root, grandparent);
+		}
+	}
+}
+
+void RightRotate(Node **rootNode, Node *newNode) // RightRotate 함수
+{
+	Node *root = (*rootNode);
+	Node *grandparent = newNode->parent->parent; //삽입된 노드의 부모의 부모를 grandparent로 설정해줍니다
+	Node *parent = newNode->parent;				 //삽입된 노드의 부모를 parent로 설정해줍니다
+
+	if (parent->right != NULL) //만약 부모의 right에 노드가 있다면
+	{
+		parent->right->parent = grandparent; //부모의 right의 부모가 grandparent를 가리키게 합니다
+	}
+
+	grandparent->left = parent->right;	  //grandparent의 left가 parent의 right를 가리키게 합니다
+	grandparent->parent = parent;		  //grandparent의 부모가 parent를 가리키게 합니다
+	parent->right = grandparent;		  //parent의 right가 grandparent를 가리키게 합니다
+	parent->parent = grandparent->parent; //parent의 parent가 grandparent의 parent를 가리키게 합니다
+
+	if (grandparent->parent == NULL) //만약 grandparent의 parent가 NULL일 경우 즉 맨위일경우
+	{
+		*rootNode = parent; //rootNode를 parent로 설정해줍니다
+	}
+	else if (grandparent == grandparent->parent->left) //만약 grandparent가 더 위의 노드 기준 왼쪽에 위치하고 있었다면
+	{
+		grandparent->parent->left = parent; //grandparent의 left가 parent를 가리키게 합니다
+	}
+	else //만약 grandparent가 더 위의 노드 기준 오른쪽에 위치하고 있었다면
+	{
+		grandparent->parent->right = parent; //grandparent의 right가 parent를 가리키게 합니다
 	}
 }
 
@@ -173,24 +231,27 @@ void LeftRotate(Node **rootNode, Node *newNode) //LeftRotate 함수
 	Node *root = (*rootNode);
 	Node *grandparent = newNode->parent->parent; //삽입된 노드의 부모의 부모를 grandparent로 설정해줍니다
 	Node *parent = newNode->parent;
-	parent->right = newNode->left;
-	grandparent->left = newNode;
-	newNode->left = parent;
-	root = grandparent;
-	InspectNode(&root, newNode->left);
-	printf("☆");
-}
 
-void RightRotate(Node **rootNode, Node *newNode) //RightRotate 함수
-{
-	Node *root = (*rootNode);
-	Node *grandparent = newNode->parent->parent; //삽입된 노드의 부모의 부모를 grandparent로 설정해줍니다
-	Node *parent = newNode->parent;
-	grandparent->left = parent->right;
-	parent->right = grandparent;
-	grandparent = parent;
-	root = grandparent;
-	grandparent->color = BLACK;
-	parent->color = BLACK;
-	printf("★");
+	if (parent->left != NULL)
+	{
+		parent->left->parent = grandparent;
+	}
+
+	grandparent->right = parent->left;
+	grandparent->parent = parent;
+	parent->left = grandparent;
+	parent->parent = grandparent->parent;
+
+	if (grandparent->parent == NULL)
+	{
+		*rootNode = parent;
+	}
+	else if (grandparent == grandparent->parent->left)
+	{
+		grandparent->parent->left = parent;
+	}
+	else
+	{
+		grandparent->parent->right = parent;
+	}
 }
